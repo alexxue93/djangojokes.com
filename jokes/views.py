c818c62413210b72ed4dc8e80f17b7222ff6ded5
AlexXue <alexxue93@gmail.com>
2024-03-27 18:59:14 -0400

    DetailView

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
 from .models import Joke
 
+class JokeDetailView(DetailView):
+    model = Joke
+    
 class JokeListView(ListView):
     model = Joke
