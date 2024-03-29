ea67236c3414a0e63da850374bd6970cbb425922
AlexXue <alexxue93@gmail.com>
2024-03-29 10:08:25 -0400

    PostgreSQL setup

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': '63840',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
