fbec246a11dc1b192bd10ef1d9b90e7916e872ad
AlexXue <alexxue93@gmail.com>
2024-04-01 18:39:28 -0400

    Adding a JokeVote Model

--- jokes/models.py
+++ jokes/models.py
@@ -8,7 +8,7 @@ class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     user = models.ForeignKey(
-    settings.AUTH_USER_MODEL, on_delete=models.PROTECT
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT
     )
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
     tags = models.ManyToManyField('Tag', blank=True)
@@ -78,3 +78,22 @@ class Tag(models.Model):
 class Meta:
     ordering = ['tag']
     
+class JokeVote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    joke = models.ForeignKey(
+        Joke, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    vote = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'joke'], name='one_vote_per_user_per_joke'
+            )
+        ]
