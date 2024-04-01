1faac47773ac4e7e0fade7f7b6877f6da014aeac
AlexXue <alexxue93@gmail.com>
2024-04-01 16:51:24 -0400

    Amazon S3 and Static Settings in Development

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,7 @@
 import filetype
 from datetime import datetime
-""" from djangojokes.storage_backends import PrivateMediaStorage """
-from private_storage.fields import PrivateFileField
+from djangojokes.storage_backends import PrivateMediaStorage
+""" from private_storage.fields import PrivateFileField """
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 from django.db import models
@@ -48,7 +48,8 @@ class Applicant(models.Model):
     available_days = models.CharField(max_length=20)
     desired_hourly_wage = models.DecimalField(max_digits=5, decimal_places=2)
     cover_letter = models.TextField()
-    resume = PrivateFileField(
+    resume = models.FileField(
+        storage = PrivateMediaStorage(),
         upload_to='resumes', blank=True, help_text='PDFs only',
         validators=[validate_pdf]
     )
