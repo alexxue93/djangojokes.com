3eb2056e09e26274b9c250302754d39058cc9c4b
AlexXue <alexxue93@gmail.com>
2024-04-02 16:42:45 -0400

    Getting Joke Ratings

--- jokes/models.py
+++ jokes/models.py
@@ -1,6 +1,7 @@
-from django.db import models
-from django.urls import reverse
 from django.conf import settings
+from django.db import models
+from django.db.models import Avg, Count, Sum
+from django.urls import reverse
 
 from common.utils.text import unique_slug
 
@@ -8,16 +9,19 @@ class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+        related_name='jokes'
     )
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    tags = models.ManyToManyField('Tag', blank=True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT, related_name='jokes'
+    )
+    tags = models.ManyToManyField('Tag', blank=True, related_name='jokes')
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     @property
     def num_votes(self):
         return self.jokevotes.count()
@@ -29,6 +33,36 @@ class Joke(models.Model):
     @property
     def num_dislikes(self):
         return self.jokevotes.filter(vote=-1).count()
+
+    @property
+    def rating(self):
+        if self.num_votes == 0: # No jokes, so rating is 0
+            return 0
+
+        r = JokeVote.objects.filter(joke=self).aggregate(average=Avg('vote'))
+
+        # Return the rounded rating.
+        return round(5 + (r['average'] * 5), 2)
+
+    @property
+    def votes(self):
+        result = JokeVote.objects.filter(joke=self).aggregate(
+            num_votes=Count('vote'),
+            sum_votes=Sum('vote')
+        )
+
+        # If there aren't any votes yet, return a dictionary with values of 0.
+        if result['num_votes'] == 0:
+            return {'num_votes': 0, 'rating': 0, 'likes': 0, 'dislikes': 0}
+
+        # Otherwise, calculate the dict values using num_votes and sum_votes.
+        result['rating'] = round(
+            5 + ((result['sum_votes']/result['num_votes'])*5), 2
+        )
+        result['dislikes'] = int((result['num_votes'] - result['sum_votes'])/2)
+        result['likes'] = result['num_votes'] - result['dislikes']
+
+        return result
 
     def get_absolute_url(self):
         return reverse('jokes:detail', args=[self.slug])
@@ -42,7 +76,8 @@ class Joke(models.Model):
 
     def __str__(self):
         return self.question
-    
+
+
 class Category(models.Model):
     category = models.CharField(max_length=50)
     slug = models.SlugField(
@@ -65,7 +100,8 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        #ordering = ['category']
+        ordering = ['category']
+
 
 class Tag(models.Model):
     tag = models.CharField(max_length=50)
@@ -86,10 +122,8 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
-    
-class Meta:
-    ordering = ['tag']
-    
+
+
 class JokeVote(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
